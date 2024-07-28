d86eac2272c26df357892b9814d2e5350b280169
thajucp123 <thajucp123@gmail.com>
2024-07-28 22:22:08 +0530

    image optimizations

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
   plugins: [react()],
   css: {
     preprocessorOptions: {
